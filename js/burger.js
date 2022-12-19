b080cd2b902e5b40c1a84b409193a390281d4890
mikhiia <annmikhaiilova@gmail.com>
2022-12-19 23:30:59 +0300

    burger-menu!!!

--- js/burger.js
+++ js/burger.js
@@ -1,26 +1,9 @@
-// function burgerMenu(selector) {
-//     let menu = $(selector);
-//     let button = menu.find('.burger-button', '.burger');
-//     let links = menu.find('.burger-menu');
-//     let overlay = menu.find('.burger-menu_overlay');
-    
-//     button.on('click', (e) => {
-//       e.preventDefault();
-//       toggleMenu();
-//     });
-    
-//     menus.on('click', () => toggleMenu());
-//     overlay.on('click', () => toggleMenu());
-    
-//     function toggleMenu(){
-//       menu.toggleClass('burger_active');
-      
-//       if (menu.hasClass('burger_active')) {
-//         $('body').css('overlow', 'hidden');
-//       } else {
-//         $('body').css('overlow', 'visible');
-//       }
-//     }
-//   }
-  
-//   burgerMenu('.burger-menu');
+const iconMenu = document.querySelector('.burger-button');
+if (iconMenu) {
+    const menuBody = document.querySelector('.header__menu');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+});
+}
